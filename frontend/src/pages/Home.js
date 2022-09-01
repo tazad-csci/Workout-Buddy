73bd883e69774a57ba9bdf30ab77ad3027a930a0
tazad-csci <tamjidazad@gmail.com>
2022-08-31 23:49:16 -0500

    Lesson09-Fetching-Data

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,36 @@
+import { useEffect, useState } from 'react';
+
+// components
+import WorkoutDetails from '../components/WorkoutDetails';
+
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      // an array of objects where each object represents a workout
+      // fetch the data and store the response in an object
+      const response = await fetch('/api/workouts');
+
+      // prase json from this response object
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
     </div>
   );
 };
